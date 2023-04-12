9c7ed8db1891dada4ac09a029f8d12cc48aa3f9f
GleysonRibeiro <89668176+GleysonRibeiro@users.noreply.github.com>
2023-04-11 22:06:48 -0300

    Update Rodada.java

--- src/br/edu/iff/jogoforca/dominio/rodada/Rodada.java
+++ src/br/edu/iff/jogoforca/dominio/rodada/Rodada.java
@@ -1,18 +1,22 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
+import java.util.List;
+
 import br.edu.iff.bancodepalavras.dominio.letra.Letra;
 import br.edu.iff.bancodepalavras.dominio.palavra.Palavra;
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
 import br.edu.iff.dominio.ObjetoDominioImpl;
 
 public class Rodada extends ObjetoDominioImpl {
 	
 	private static final int maxPalavras = 3;
-	private static final int maxErros = 0;
+	private static final int maxErros = 10;
 	private static final int pontosQuandoDescobreTodasAsPalavras = 100;
 	private static final int pontosPorLetraEncoberta = 15;
 	private BonecoFactory bonecoFactory;
 	private Item[] itens;
 	private Jogador jogador;
+	private List<Letra> letrasErradas = null;
 	
 	
 	public static int getMaxPalavras() {
@@ -72,5 +76,42 @@ public class Rodada extends ObjetoDominioImpl {
 		this.jogador = jogador;
 	}
 	
-	//continuar implementação a partir daqui, tratar anotações do diagrama
+	public Jogador getJogador() {
+		return this.jogador;
+	}
+	
+	public Tema getTema() {
+		return itens[0].getPalavra().getTema();
+	}
+	
+	public Palavra[] getPalavras() {
+		Palavra[] palavras = null;
+		for(int i = 0; i<itens.length;i++) {
+			palavras[i] = itens[i].getPalavra();			
+		}
+		
+		return palavras;
+	}
+	
+	public int getNumPalavras() {
+		int cont = 0;
+		for(int i = 0;i<itens.length;i++) {
+			if(itens[i]!=null) {
+				cont++;
+			}
+		}
+		
+		return cont;
+	}
+	
+	public void tentar(char codigo) {
+		
+		for(int i = 0; i<this.getNumPalavras();i++) {
+			if(this.itens[i].tentar(codigo)==false) {
+								
+			}
+		}
+		
+		
+	}
 }
